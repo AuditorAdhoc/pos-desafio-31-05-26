6986f2587157c3ee685ecab3885797dcebf904c3
AuditorAdhoc <auditor.adhoc@gmail.com>
2026-05-22 18:07:50 -0300

    Alteração

--- test/ServicoDePagamento.test.js
+++ test/ServicoDePagamento.test.js
@@ -6,7 +6,7 @@ describe('Classe de serviço de pagamento', function () {
   it('Validar que um pagamento MAIOR que R$ 100,00 reais é caro', function () {
 
     const servicoDePagamento = new ServicoDePagamento();
-    servicoDePagamento.pagar('0987-7656-3475', 'Samar', 156.87);
+    servicoDePagamento.pagar('1234-5678-9101', 'Samsung', 145.99);
     const retorno = servicoDePagamento.consultarUltimoPagamento();
     assert.equal(retorno.categoria, 'cara');
 
@@ -15,7 +15,7 @@ describe('Classe de serviço de pagamento', function () {
   it('Validar que um pagamento MENOR ou IGUAL a R$ 100,00 reais é padrão', function () {
 
     const servicoDePagamento = new ServicoDePagamento();
-    servicoDePagamento.pagar('1111-2222-3333', 'Empresa XPTO', 99.99);
+    servicoDePagamento.pagar('1234-5678-9101', 'Samsung', 99.99);
     const retorno = servicoDePagamento.consultarUltimoPagamento();
     assert.equal(retorno.categoria, 'padrão');
 
@@ -24,10 +24,10 @@ describe('Classe de serviço de pagamento', function () {
   it('Validar método de consultar último pagamento', function () {
 
     const servicoDePagamento = new ServicoDePagamento();
-    servicoDePagamento.pagar('0001', 'Empresa A', 50.00);
-    servicoDePagamento.pagar('0002', 'Empresa B', 250.00);
+    servicoDePagamento.pagar('0001', 'Xiaomi', 76.00);
+    servicoDePagamento.pagar('0002', 'Apple', 299.00);
     const retorno = servicoDePagamento.consultarUltimoPagamento();
-    assert.equal(retorno.valor, 250.00);
+    assert.equal(retorno.valor, 299.00);
 
   });
 
